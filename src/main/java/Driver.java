bea0b91b92a39b916802e9b7744c4d15ad124dc9
Raffa Rizky Febryan <raffarizkyf@gmail.com>
2026-05-28 21:02:12 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,6 +5,8 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
+
+		public static String IP_ADDRESS = "192.168.1.1";
 		
 		System.out.println("Current count:" + counter.getCount());
 		
